28551e06efeb1901c96b70087027e17f6eab6822
NikhilPandey2025 <nikhilkanttpandey@gmail.com>
2025-01-05 23:59:25 +0000

    datatypes

---
+++ Basics/datatypes.js
@@ -0,0 +1,19 @@
+// alert(3+3)   .... we are using nodejs not brower
+
+let name ="hitesh"
+let age =18
+let isLoggedIn = false
+
+/*
+number 
+bit
+string  =""
+boolean = true,false
+null 
+undefined
+symbol = unique
+*/
+
+//object 
+
+console.log(typeof age)

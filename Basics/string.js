5971eeeb60da9fd227652558fa574e87a4b26432
NikhilPandey2025 <nikhilkanttpandey@gmail.com>
2025-01-06 01:16:11 +0000

    String

---
+++ Basics/string.js
@@ -0,0 +1,17 @@
+const name ="Nikhil"
+const repoCount= 50
+
+console.log(name + repoCount);
+console.log(`hello my name ${name} and my repo count is ${repoCount}`);
+
+ const gameName = new String("Pandey")
+ console.log(gameName[0]);
+ console.log(gameName.__proto__);
+ console.log(gameName.length);
+ console.log(gameName.toLowerCase());
+console.log(gameName.charAt(3));
+ console.log(gameName.indexOf('y'));
+ 
+ 
+ // Sclics,Trim,replace,includes  (theory Part)
+ 

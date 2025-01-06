efd6da93efe2186f7c9697ed185e49c2bd65cc8c
NikhilPandey2025 <nikhilkanttpandey@gmail.com>
2025-01-06 01:15:41 +0000

    Math and Num

---
+++ Basics/num and math.js
@@ -0,0 +1,21 @@
+const score =400
+console.log(score);
+
+
+const balance = new Number(100)
+console.log(balance);
+console.log(balance.toPrecision(2));
+console.log(balance.toString());
+
+//++++++++++++++++  Math ++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.ceil(4.2));
+console.log(Math.random());
+
+const min =10
+const max =20
+
+console.log(Math.random() * (max-min +1)+min);
+

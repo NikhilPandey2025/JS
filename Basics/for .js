f226cb46e093d7f1ab08b60e34088317af8df94f
NikhilPandey2025 <nikhilkanttpandey@gmail.com>
2025-01-08 10:08:13 +0000

    for Loop

---
+++ Basics/for .js
@@ -0,0 +1,27 @@
+// // for
+//  for (let index = 0; index <= 10; index++) {
+//     const element = index;
+//     console.log(element);
+    
+//  }
+
+//  let array = ["flah", "batman","super"]
+
+//  for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+//     console.log(element);
+    
+    
+//  }
+
+ // break ,continue 
+  for (let index = 1; index <=20; index++) {
+    if (index==5) {
+        console.log(`detected 5 `);
+       continue
+       //  break
+    }
+    console.log(`value of i is ${index}`);
+    
+    
+  }

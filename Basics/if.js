25ca8fe6fb84e447964c5061b84d0db2085b3198
NikhilPandey2025 <nikhilkanttpandey@gmail.com>
2025-01-08 09:45:13 +0000

    if condition

---
+++ Basics/if.js
@@ -0,0 +1,34 @@
+//if - >,<,<=,>=,==,===,!=,!==
+ 
+if(2==="2"){
+  console.log(true);
+  
+}
+
+const balance =1000
+
+if(balance>500) console.log("test");
+
+//nested 
+if(balance>500){
+    console.log("less than");
+    
+}else if(balance<750){
+    console.log("less than 750");
+    
+}
+else{
+    console.log("less than 1200");
+    
+}
+
+ const userlogg =true
+ const  debit = true
+
+ if(userlogg && debit && 2==3){
+  console.log("allow");
+  
+ }
+
+
+

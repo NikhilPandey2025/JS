327570c2abbca966c1987691c9029c522301128f
NikhilPandey2025 <nikhilkanttpandey@gmail.com>
2025-01-08 09:44:56 +0000

    truthy

---
+++ Basics/truthy.js
@@ -0,0 +1,25 @@
+const email = "ggfgsjg.ai"
+
+if(email){
+    console.log("got email");
+}
+else{
+    console.log("dont");
+    
+}
+
+// tuthy - "0",'false'," space given " ,[],{},function(){}
+// falsy - false,0,-0,bitInt 0n,null ,undefined ,NAN,""
+//array.length ==0
+// obj.key((emptyobj).length ===0)
+
+//nullish (??)
+let val1;
+// val1 =5 ?? 10
+val1= null ?? 10
+console.log(val1);
+
+
+// ternary 
+// condition ? true :false
+

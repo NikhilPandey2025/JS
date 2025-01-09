b2784d7e6380563b026cf0878b31d68c8be65c00
NikhilPandey2025 <nikhilkanttpandey@gmail.com>
2025-01-09 16:14:14 +0000

    for_Each

---
+++ intermediate/foreach.js
@@ -0,0 +1,81 @@
+const coding =["java","css","js","ruby","dgfys"]
+
+// coding.forEach(function(item) {
+//     console.log(item);
+    
+// })
+
+// second way 
+/*coding.forEach((item)=>{
+    console.log(item);
+    
+})*/
+
+// / third way 
+/*function printme(item) {
+    console.log(item);
+    
+}
+coding.forEach(printme)*/
+
+// fourth 
+
+/*coding.forEach((item,index,arr)=>{
+    console.log(item,arr,index);
+    
+})*/
+
+// fifth
+
+/*const myCode =[
+    {
+        language: "jass",
+        languagefile:"js"
+    },
+
+    {
+        language: "jasguis",
+        languagefile:"jvvv"
+    },
+
+    {
+        language: "jassfet",
+        languagefile:"jsrtttt"
+    }
+]
+myCode.forEach((item)=>{
+      console.log(item.language);
+      
+})
+      */
+
+// const value = coding.forEach((item)=>{
+//     console.log(item);
+    
+// })
+// console.log(value);
+// forech never return a value 
+
+
+// FILTER - Return a Value 
+
+/* ist 
+const number =[1,2,3,4,5,6,778,7]
+number.filter((num)=>{
+   return num>4
+})
+console.log(number);
+// use callback come and then you give a condtion and if condition satistfied then work 
+
+*/
+/*
+const newg=[]
+newg.forEach((item)=>{
+    if (item>4) {
+        newg.push(item)
+    }
+})
+console.log(newg);
+
+*/
+  

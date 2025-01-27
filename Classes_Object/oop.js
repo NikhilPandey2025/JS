52973ca034035ae9a459927dba69db484cf61926
NikhilPandey2025 <nikhilkanttpandey@gmail.com>
2025-01-27 06:49:02 +0000

    Object

---
+++ Classes_Object/oop.js
@@ -0,0 +1,20 @@
+const user = {
+    user : "hitesh",
+    logn : 8 ,
+    sign : true ,
+
+     getUser : function(){
+        console.log("get user detail ");
+        
+     }
+
+
+
+
+}
+
+// console.log(user.sign);
+
+// const promiseOne = new Promise((resolve, reject) => {
+    
+// })

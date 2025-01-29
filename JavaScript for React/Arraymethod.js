6fa86c407ea34302f94577a2c8f62a700c019518
NikhilPandey2025 <nikhilkanttpandey@gmail.com>
2025-01-29 13:05:47 +0000

    ArrayMethod

---
+++ JavaScript for React/Arraymethod.js
@@ -0,0 +1,38 @@
+
+// // map
+//  const number =[10,20,30]
+// const result = number.map(function(n){
+//        return n+10;
+// })
+
+// console.log(result);
+
+// // filter 
+// const numbers=[10,20,30,40,50,60];
+// const results  =numbers.filter((spiderman)=>{
+//        return spiderman>30;
+
+// })
+// console.log(results);
+
+
+// const categories = ['mobile','laptops','smartwatch']
+
+// const result =categories.filter((cat)=>cat =='mobile')
+// console.log(result);
+
+
+// exampleas 2 
+
+
+const categories = [
+    {
+    mobile: 'samsung f2',
+    price:'10000'
+     }, 
+{
+
+}]
+
+const result =categories.filter((cat)=>cat =='mobile')
+console.log(result);

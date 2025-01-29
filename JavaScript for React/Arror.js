f2ceb8bd37904d6050b7c5a1382dad40dd9f78d1
NikhilPandey2025 <nikhilkanttpandey@gmail.com>
2025-01-29 13:05:30 +0000

    arror

---
+++ JavaScript for React/Arror.js
@@ -0,0 +1,21 @@
+function sum( ) {
+    console.log("hello");
+    
+}
+sum()
+
+// arrow 
+
+const print = ()=>{
+    console.log("nikhil");
+    
+}
+
+// take parameter 
+
+const sums =(a,b)=>{
+    return a + b;
+}
+ const pandey = sum(6,4)
+ console.log(pandey);
+ 

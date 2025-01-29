81fa3601bd3e53ed2126353050706ebec63ef881
NikhilPandey2025 <nikhilkanttpandey@gmail.com>
2025-01-29 13:06:11 +0000

    Ternary

---
+++ JavaScript for React/ternary.js
@@ -0,0 +1,14 @@
+const age = 20;
+
+if (age>=18) {
+    //  console.log("you can now drive a car");
+     
+} else {
+    // console.log("you cant able to drive ");
+    
+}
+
+// use ternary for short
+// (condition)? execute : other that execute
+(age>=18) ? console.log("you can drive a car"):console.log("you cant able to drive");
+
